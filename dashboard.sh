c606ca2ef20aa5acf4d7bee9dd5c692f0f17acbf
pealock <andrewpealock@raiden.local>
2024-06-12 09:28:23 -0400

    updated dashboard.sh to allow custom website input

--- dashboard.sh
+++ dashboard.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 # Launches dashboard
 
+echo "Where would you like to go?"
+echo
+echo
+echo
+
+sleep 1
+
+read -rp "Target website:" destination
+echo -e "DESTINATION='$destination'" > ~/dashtop/.env
+echo
+echo
+
+sleep 3
 echo "Launching dashboard"
 echo
 echo
