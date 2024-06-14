392da74b832e95069e09f69dfec86b88fb5e8411
pealock <andrewpealock@raiden.local>
2024-06-14 12:19:52 -0400

    removed full screen option for testing

--- scripts/chromeOptions.py
+++ scripts/chromeOptions.py
@@ -7,7 +7,7 @@ prefs = {"credentials_enable_service": False, "profile.password_manager_enabled"
 options.add_experimental_option("detach", True)
 options.add_experimental_option("excludeSwitches", ['enable-automation'])
 options.add_experimental_option("prefs", prefs)
-options.add_argument("--kiosk")
+# options.add_argument("--kiosk")
 # options.add_argument("--start-maximized")
 options.add_argument('--force-dark-mode')
 
